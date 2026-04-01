d211469274fc9eafdc2d8ae4d0f7ff96b0b88e50
poornakani <kanishkapoorna@gmail.com>
2026-04-01 19:56:32 +0100

    Fix missing useMemo import in Bestselling.jsx

--- src/HomePage/Bestselling.jsx
+++ src/HomePage/Bestselling.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import QuickViewModal from "@/Widgets/QuickViewModal";
